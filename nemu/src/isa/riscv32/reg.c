c43a92337a7e4e5785defb34ea605b6ffbc717a0
Thomation <thomation@126.com>
2023-02-18 10:34:33 +0800

    Show pc value

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -39,6 +39,10 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  if(strcmp(s, "pc") == 0) {
+    *success = true;
+    return cpu.pc;
+  }
   int len = regs_count();
   for(int i = 0; i < len; i ++) {
     if(strcmp(s, regs[i]) == 0) {
