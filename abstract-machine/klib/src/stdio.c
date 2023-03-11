70ce4928867bc487c620cde06090627eef7bff56
Thomation <thomation@126.com>
2023-03-11 10:22:07 +0800

    implement sprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,13 +8,78 @@
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
+#define MAX_INT_STR_LEN 32
+int uint_to_str(unsigned int n, char * str)
+{
+  char tmp[MAX_INT_STR_LEN];
+  int i = 0;
+  while(n > 0)
+  {
+    tmp[i ++] = n % 10 + '0';
+    n /= 10;
+  }
+  int j = 0;
+  while(i > 0)
+  {
+    str[j++] = tmp[--i];
+  }
+  str[j] = '\0';
+  return j;
+}
+int int_to_str(int n, char * str)
+{
+  if(n > 0)
+    return uint_to_str(n, str);
+  char tmp[MAX_INT_STR_LEN];
+  int i = 0;
+  while(n != 0)
+  {
+    tmp[i++] = - (n % 10) + '0';
+    n /= 10;
+  }
+  str[0] = '-';
+  int j = 1;
+  while(i > 0)
+  {
+    str[j++] = tmp[--i];
+  }
+  str[j] = '\0';
+  return j;
+}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char * cur = out;
+  while(*fmt) {
+    if(*fmt == '%') {
+      fmt ++;
+      switch(*fmt ++) {
+        case 'd':
+          int d = va_arg(ap, int);
+          int len = int_to_str(d, cur);
+          cur += len;
+        break;
+        case 's':
+          char *s = va_arg(ap, char*);
+          strcpy(cur, s);
+          cur += strlen(s);
+        break;
+        default:
+          panic("Unsupported");
+      }
+    }
+    else{
+      *cur ++ = *fmt ++;
+    }
+  }
+  return cur - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
