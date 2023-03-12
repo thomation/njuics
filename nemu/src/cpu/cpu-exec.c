7e8ef83b79af0c3316d2c4a2a960d3928aa84cdc
Thomation <thomation@126.com>
2023-03-12 08:34:40 +0800

    remove useless log

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -38,9 +38,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  if(check_wp()) {
-    nemu_state.state = NEMU_STOP;
-  }
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
@@ -76,6 +73,9 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
+    if(check_wp()) {
+      nemu_state.state = NEMU_STOP;
+    }
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
