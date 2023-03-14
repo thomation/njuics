9a53a344bc9b4a3d8d291d06d22f1d7fcc6d6733
Thomation <thomation@126.com>
2023-03-14 09:22:15 +0800

    read elf file

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,6 +1,12 @@
 
 #include <common.h>
 
+FILE *elf_fp = NULL;
 void init_elf(const char *elf_file) {
-  Log("Elf is from %s", elf_file ? elf_file : "Error");
+  if (elf_file != NULL) {
+    FILE *fp = fopen(elf_file, "r");
+    Assert(fp, "Can not open '%s'", elf_file);
+    elf_fp = fp;
+    Log("Elf is from %s", elf_file);
+  }
 }
