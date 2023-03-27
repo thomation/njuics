3e387cf5d0b04d97a7c822ff1ab0a9589ee56c78
Thomation <thomation@126.com>
2023-03-28 07:52:02 +0800

    Add bool test from dry.c

---
+++ am-kernels/tests/cpu-tests/tests/bool.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include "trap.h"
+
+typedef int     Boolean;
+int             Int_Glob;
+Boolean         Bool_Glob;
+char            Ch_1_Glob,
+                Ch_2_Glob;
+void Proc_4 () /* without parameters */
+/*******/
+    /* executed once */
+{
+  Boolean Bool_Loc;
+
+  Bool_Loc = Ch_1_Glob == 'A';
+  Bool_Glob = Bool_Loc | Bool_Glob;
+  Ch_2_Glob = 'B';
+} /* Proc_4 */
+
+
+void Proc_5 () /* without parameters */
+/*******/
+    /* executed once */
+{
+  Ch_1_Glob = 'A';
+  Bool_Glob = false;
+} /* Proc_5 */
+
+void main()
+{
+    Proc_5();
+    Proc_4();
+    check(Bool_Glob == 1);
+}
