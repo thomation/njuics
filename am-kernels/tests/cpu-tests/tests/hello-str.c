5d58d70c047b6b696ba0233c9f176bd8cdc62a3c
Thomation <thomation@126.com>
2023-03-28 09:26:15 +0800

    Add code to hello-str to test. Maybe compare bug

--- am-kernels/tests/cpu-tests/tests/hello-str.c
+++ am-kernels/tests/cpu-tests/tests/hello-str.c
@@ -4,9 +4,25 @@ typedef char    Str_30 [31];
 char buf[128];
 Str_30  Str_1_Loc;
 Str_30  Str_2_Loc;
+int Func_2 (Str_1_Par_Ref, Str_2_Par_Ref)
+/*************************************************/
+    /* executed once */
+    /* Str_1_Par_Ref == "DHRYSTONE PROGRAM, 1'ST STRING" */
+    /* Str_2_Par_Ref == "DHRYSTONE PROGRAM, 2'ND STRING" */
+
+Str_30  Str_1_Par_Ref;
+Str_30  Str_2_Par_Ref;
+{
+	// printf("s1:%s\n", Str_1_Par_Ref);
+	// printf("s2:%s\n", Str_2_Par_Ref);
+  if (strcmp (Str_1_Par_Ref, Str_2_Par_Ref) > 0)
+    return 1;
+  return 0;
+}
 int main() {
 	strcpy (Str_1_Loc, "DHRYSTONE PROGRAM, 1'ST STRING");
 	strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
+	check(!Func_2(Str_1_Loc, Str_2_Loc));
 	check(strcmp(Str_1_Loc, Str_2_Loc) < 0);
 	sprintf(buf, "%s", "Hello world!\n");
 	check(strcmp(buf, "Hello world!\n") == 0);
