4bd334697d911b1bd42ab28092e6697657c1723b
Thomation <thomation@126.com>
2023-03-28 09:13:42 +0800

    Copy code from dry.c to hello-str

--- am-kernels/tests/cpu-tests/tests/hello-str.c
+++ am-kernels/tests/cpu-tests/tests/hello-str.c
@@ -1,8 +1,13 @@
 #include "trap.h"
 
+typedef char    Str_30 [31];
 char buf[128];
-
+Str_30  Str_1_Loc;
+Str_30  Str_2_Loc;
 int main() {
+	strcpy (Str_1_Loc, "DHRYSTONE PROGRAM, 1'ST STRING");
+	strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
+	check(strcmp(Str_1_Loc, Str_2_Loc) < 0);
 	sprintf(buf, "%s", "Hello world!\n");
 	check(strcmp(buf, "Hello world!\n") == 0);
 
