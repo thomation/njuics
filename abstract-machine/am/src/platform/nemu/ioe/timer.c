f225b1efc9dacdea6af63647784a56455c782535
Thomation <thomation@126.com>
2023-03-29 09:07:01 +0800

    Correct timer bug

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,15 +4,16 @@
 
 static uint32_t boot_time[2];
 void __am_timer_init() {
+  boot_time[1] = inl(RTC_ADDR + 4); 
   boot_time[0] = inl(RTC_ADDR);
-  boot_time[1] = inl(RTC_ADDR + 4);
   printf("boot time:%d, %d\n", boot_time[0], boot_time[1]);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t new_time[2];
-  new_time[0] = inl(RTC_ADDR);
+  // Call offset==4 first which is the implement of timer.c in device folder. I don't know why it is write like this.
   new_time[1] = inl(RTC_ADDR + 4);
+  new_time[0] = inl(RTC_ADDR);
   uint64_t s = new_time[1] - boot_time[1];
   uptime->us = (s << 32)  + (new_time[0] - boot_time[0] + 500);
   // printf("time:%d, %d\n", new_time[0], new_time[1]);
