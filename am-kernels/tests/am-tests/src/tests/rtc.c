a940c7473c2f30c205b74a5c136c9b379f1898de
Thomation <thomation@126.com>
2023-03-29 08:02:27 +0800

    Add test of delta time

--- am-kernels/tests/am-tests/src/tests/rtc.c
+++ am-kernels/tests/am-tests/src/tests/rtc.c
@@ -1,8 +1,14 @@
 #include <amtest.h>
+#define Start_Timer() Begin_Time = uptime_ms()
+#define Stop_Timer()  End_Time   = uptime_ms()
 
+static uint32_t uptime_ms() { return io_read(AM_TIMER_UPTIME).us / 1000; }
+static uint32_t Begin_Time;
+static uint32_t End_Time;
 void rtc_test() {
   AM_TIMER_RTC_T rtc;
   int sec = 1;
+  Start_Timer();
   while (1) {
     while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
     rtc = io_read(AM_TIMER_RTC);
@@ -13,5 +19,8 @@ void rtc_test() {
       printf("%d seconds).\n", sec);
     }
     sec ++;
+    Stop_Timer();
+    int delta = End_Time - Begin_Time;
+    printf("delta:%d\n", delta);
   }
 }
