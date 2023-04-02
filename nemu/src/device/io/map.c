c1da815e91d564e518ffe9888b0189a7a012ef86
Thomation <thomation@126.com>
2023-04-02 09:06:11 +0800

    Add dtrace

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef CONFIG_DTRACE_COND
+  if (DTRACE_COND) {log_write("map_read:%0x, len:%d, device:%s\n", addr, len, map->name); }
+#endif
   return ret;
 }
 
@@ -66,5 +69,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+#ifdef CONFIG_DTRACE_COND
+  if (DTRACE_COND) {log_write("map_write:%0x, len:%d, device:%s\n", addr, len, map->name); }
+#endif
   invoke_callback(map->callback, offset, len, true);
 }
