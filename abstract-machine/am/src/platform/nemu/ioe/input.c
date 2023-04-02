8ea15f8df6b7f6ec1adb578ad78b5ae348e90fe2
Thomation <thomation@126.com>
2023-04-02 09:50:45 +0800

    Add keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,12 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  // It is available on nemu window is active
+  uint32_t k = inl(KBD_ADDR); 
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
