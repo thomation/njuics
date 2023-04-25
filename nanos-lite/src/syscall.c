f45f15d16c09305c3eca369b760b946a93c65d85
Thomation <thomation@126.com>
2023-04-26 07:28:43 +0800

    print log of system call

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,7 +8,7 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
+  printf("System Call: %d\n", a[0]);
   switch (a[0]) {
     case SYS_yield:
       c->GPRx = sys_yield();
