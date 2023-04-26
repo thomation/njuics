2392eb48f77611f8e0a803492d33980c6a10c0e0
Thomation <thomation@126.com>
2023-04-26 08:50:53 +0800

    Add sys_write with bug of printf

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,21 +8,16 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     // printf("am_irq_handle:%d\n", c->mcause);
-    switch (c->mcause) {
-      case -1:
-        ev.event = EVENT_YIELD;
-        break;
-      case 0:
-      case 1:
-        ev.event = EVENT_SYSCALL;
-        break;
-      default: ev.event = EVENT_ERROR; break;
-    }
+    if(c->mcause == -1)
+      ev.event = EVENT_YIELD;
+    else if(c->mcause >= 0 && c->mcause < 20)
+      ev.event = EVENT_SYSCALL;
+    else
+      ev.event = EVENT_ERROR;
 
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-
   return c;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,19 +2,24 @@
 #include "syscall.h"
 int sys_yield();
 int sys_exit();
+int sys_write(int fd, void * buf, size_t len);
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  printf("System Call: %d\n", a[0]);
+  printf("System Call: type:%d, a1:%p, a2:%p, a3:%p\n", a[0], a[1], a[2], a[3]);
   switch (a[0]) {
     case SYS_yield:
       c->GPRx = sys_yield();
+      break;
     case SYS_exit:
       c->GPRx = sys_exit();
-    break;
+      break;
+    case SYS_write:
+      c->GPRx = sys_write(a[1], (void*)a[2], a[3]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
@@ -27,4 +32,14 @@ int sys_exit()
 {
   halt(0);
   return 0;
+}
+int sys_write(int fd, void* buf, size_t len) {
+  uint8_t *str = buf;
+  if(fd == 1) {
+    for(int i = 0; i < len; i ++) {
+      putch(str[i]);
+    }
+    return len;
+  }
+  return -1;
 }
