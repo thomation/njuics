5ae3a5fd6852ce2ef5e87bf617e274520c5e0093
Thomation <thomation@126.com>
2023-04-28 09:49:45 +0800

    Load with fs methods

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,8 @@
 #include <fs.h>
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -7,6 +10,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,12 +29,51 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
-
+#define FILE_TABLE_SIZE sizeof(file_table) / sizeof(Finfo)
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  for(int i = 3; i < FILE_TABLE_SIZE; i++) {
+    Finfo *e = &file_table[i];
+    e->open_offset = 0;
+    e->read = ramdisk_read;
+    e->write = ramdisk_write;
+  }
+}
+int fs_open(const char *pathname, int flags, int mode) {
+  for(int i = 0; i < FILE_TABLE_SIZE; i ++) {
+    if(strcmp(pathname, file_table[i].name) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+size_t fs_read(int fd, void *buf, size_t len) {
+  Finfo * e = &file_table[fd];
+  return e->read(buf, e->disk_offset + e->open_offset, len);
+}
+size_t fs_write(int fd, const void *buf, size_t len) {
+  return 0;
+}
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  Finfo * e = &file_table[fd];
+  switch(whence) {
+    case SEEK_SET:
+      e->open_offset = e->disk_offset + offset;
+      break;
+    case SEEK_CUR:
+      e->open_offset += offset;
+      break;
+    case SEEK_END:
+      e->open_offset = e->disk_offset + e->size + offset;
+      break;
+  }
+  return e->open_offset;
+}
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,18 +11,24 @@
 #endif
 
 extern uint8_t ramdisk_start;
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len); 
 static void load_segment(size_t offset, uint8_t * vaddr, size_t file_size, size_t mem_size) {
     memcpy(vaddr, &ramdisk_start + offset, file_size);
     memset(vaddr + file_size, 0, mem_size - file_size);
 }
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  if(filename != NULL)
-    Log("Start load %s\n", filename);
-  else
-    Log("Start load default \n");
+  if(filename == NULL) {
+    Log("No file name!\n");
+    return 0;
+  }
+  Log("Start load %s\n", filename);
+  int fd = fs_open(filename, 0, 0);
+  if(fd < 0) {
+    Log("Cannot open file:%s\n", filename);
+    return 0;
+  }
+
   Elf_Ehdr elf_header;
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+  fs_read(fd, &elf_header, sizeof(Elf_Ehdr));
   if(elf_header.e_ident[EI_MAG0] == ELFMAG0 &&
           elf_header.e_ident[EI_MAG1] == ELFMAG1 &&
           elf_header.e_ident[EI_MAG2] == ELFMAG2 &&
@@ -29,9 +36,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     uintptr_t start = (uintptr_t)&ramdisk_start;
     Log("start:%p, entry:%x\n", start, elf_header.e_entry);
     Elf_Phdr elf_program_header;
-    // TODO: Copy all byts of segment of LOAD to memory.
     for(int i = 0; i < elf_header.e_phnum; i ++) {
-      ramdisk_read(&elf_program_header, elf_header.e_phoff + i * elf_header.e_phentsize,elf_header.e_phentsize);
+      fs_lseek(fd, elf_header.e_phoff + i * elf_header.e_phentsize, SEEK_SET);
+      fs_read(fd, &elf_program_header, elf_header.e_phentsize);
       if(elf_program_header.p_type == PT_LOAD) {
         load_segment(elf_program_header.p_offset, (uint8_t*)elf_program_header.p_vaddr, elf_program_header.p_filesz, elf_program_header.p_memsz);
       }
