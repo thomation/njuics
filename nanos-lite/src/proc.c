e8f3f0b516e2dc33100c3c6bf785610427e5636d
Thomation <thomation@126.com>
2023-05-16 09:09:15 +0800

    Implement bird

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
 
   // load program here
 
-  naive_uload(NULL, "/bin/nterm");
+  naive_uload(NULL, "/bin/bird");
 }
 
 Context* schedule(Context *prev) {
--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,21 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  printf("IMG_Load file:%s\n", filename);
+  FILE * f = fopen(filename, "r");
+  fseek(f, 0, SEEK_END);
+  size_t size = f->_offset;
+  printf("IMG_Load size:%u\n", size);
+  uint8_t * buf = (uint8_t*)malloc(sizeof(uint8_t) * size);
+  fseek(f, 0, SEEK_SET);
+  size_t len = fread(buf, 1, size, f);
+  printf("IMG_load read len:%u\n", len);
+  assert(size == len);
+  SDL_Surface * surface = STBIMG_LoadFromMemory(buf, len);
+  printf("IMG_load surface %p\n", surface);
+  free(buf);
+  fclose(f);
+  return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
