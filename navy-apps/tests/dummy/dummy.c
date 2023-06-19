4284873994ad88ec2d71d4c23abfd7e89b7cc2e3
Thomation <thomation@126.com>
2023-06-19 16:49:25 +0800

    printf addr of dummy to prove it is on user space

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
@@ -8,5 +9,8 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+  int test = 10;
+  printf("dummy main addr %p\n", main);
+  printf("dummy test addr %p, value %d\n", &test, test);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
