a5f73125d4a10a72aefddc3722f1ff5df317156d
Thomation <thomation@126.com>
2023-06-22 11:32:39 +0800

    open close intr

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,15 +18,17 @@
 
 #define IRQ_TIMER 0x80000007  // for riscv32
 #define MIE_BIT 3
-#define MIE_MASK 1 << MIE_BIT
+#define MIE_MASK (1 << MIE_BIT)
 #define MPIE_BIT 7
-#define MPIE_MASK 1 << MPIE_BIT
-#define MIE_TO_MPIE(v) ((v & ~MPIE_MASK) | (v & MIE_BIT) << (MPIE_BIT - MIE_BIT))
-#define MPIE_TO_MIE(v) ((v & ~MIE_MASK) | (v & MPIE_BIT) >> (MPIE_BIT - MIE_BIT))
+#define MPIE_MASK (1 << MPIE_BIT)
+#define MIE_TO_MPIE(v) ((v & ~MPIE_MASK) | ((v & MIE_MASK) << (MPIE_BIT - MIE_BIT)))
+#define MPIE_TO_MIE(v) ((v & ~MIE_MASK) | ((v & MPIE_MASK) >> (MPIE_BIT - MIE_BIT)))
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-cpu.mstatus = MIE_TO_MPIE(cpu.mstatus);
-cpu.mstatus &= ~MIE_MASK;
+  // printf("isa_raise_intr before close intr cpu.mstatus %x\n", cpu.mstatus);
+  cpu.mstatus = MIE_TO_MPIE(cpu.mstatus);
+  cpu.mstatus &= ~MIE_MASK;
+  // printf("isa_raise_intr after close intr cpu.mstatus %x\n", cpu.mstatus);
 #ifdef CONFIG_ETRACE_COND
   if (ETRACE_COND)
     log_write("isa_raise_intr@%x, cause:%x\n", epc, NO);
@@ -50,7 +52,9 @@ word_t isa_handle_mret() {
   if (ETRACE_COND)
     log_write("mret@%x, next:%x\n", cpu.pc, addr);
 #endif
+  // printf("isa_handle_mret before open intr cpu.mstatus %x\n", cpu.mstatus);
   cpu.mstatus = MPIE_TO_MIE(cpu.mstatus);
   cpu.mstatus |= MPIE_MASK;
+  // printf("isa_handle_mret after open intr cpu.mstatus %x\n", cpu.mstatus);
   return addr;
 }
