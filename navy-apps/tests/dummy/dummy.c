0647592de744a086e86ec2d25d303e7e6967ef1f
Thomation <thomation@126.com>
2023-06-25 12:59:51 +0800

    update dummy for test

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -8,9 +8,13 @@
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
-int main() {
+int main(int argc, char ** argv) {
   int test = 10;
   printf("dummy main addr %p\n", main);
   printf("dummy test addr %p, value %d\n", &test, test);
+  while(1) {
+    printf("dummy argv = %s\n", argv[1]);
+    _syscall_(SYS_yield, 0, 0, 0);
+  }
   return _syscall_(SYS_yield, 0, 0, 0);
 }
